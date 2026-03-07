f399b425e2c2f5d74bcbcd93ff0565ebca2ccd76
Brown-Brother14 <jai.mistry14@gmail.com>
2026-03-06 23:09:02 -0700

    Update proxy.js

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -3,49 +3,28 @@ const https = require('https');
 exports.handler = async function(event) {
   const path = event.path || '';
   const params = event.queryStringParameters || {};
-
   let fullUrl = '';
 
-  if (path.includes('polygon')) {
-    const polygonPath = path.replace(/.*\/api\/polygon\//, '').replace(/.*proxy\/polygon\//, '');
-    const query = Object.keys(params).length
-      ? '?' + new URLSearchParams(params).toString()
-      : '';
-    fullUrl = `https://api.polygon.io/${polygonPath}${query}`;
-  } else if (path.includes('alpha')) {
-    const query = Object.keys(params).length
-      ? '?' + new URLSearchParams(params).toString()
-      : '';
-    fullUrl = `https://www.alphavantage.co/query${query}`;
+  if (path.includes('alpha')) {
+    const qs = Object.keys(params).length ? '?' + new URLSearchParams(params).toString() : '';
+    fullUrl = `https://www.alphavantage.co/query${qs}`;
   } else {
-    // Log what we received to help debug
-    return {
-      statusCode: 400,
-      headers: { 'Access-Control-Allow-Origin': '*' },
-      body: JSON.stringify({ error: 'Invalid route', path: path, params: params })
-    };
+    return { statusCode: 400, headers: {'Access-Control-Allow-Origin':'*'}, body: JSON.stringify({ error: 'Invalid route', path }) };
   }
 
   return new Promise((resolve) => {
     https.get(fullUrl, (res) => {
       let data = '';
       res.on('data', chunk => data += chunk);
-      res.on('end', () => {
-        resolve({
-          statusCode: 200,
-          headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Content-Type': 'application/json'
-          },
-          body: data
-        });
-      });
-    }).on('error', (e) => {
-      resolve({
-        statusCode: 500,
-        headers: { 'Access-Control-Allow-Origin': '*' },
-        body: JSON.stringify({ error: e.message, url: fullUrl })
-      });
-    });
+      res.on('end', () => resolve({
+        statusCode: 200,
+        headers: { 'Access-Control-Allow-Origin': '*', 'Content-Type': 'application/json' },
+        body: data
+      }));
+    }).on('error', (e) => resolve({
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+      body: JSON.stringify({ error: e.message })
+    }));
   });
 };
